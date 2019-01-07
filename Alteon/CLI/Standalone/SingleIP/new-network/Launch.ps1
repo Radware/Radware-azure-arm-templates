4f3004150f5eccf39b003c4eea1da0c6173a5b72
Or Zidkani <31033101+OrZidkani@users.noreply.github.com>
2019-01-07 11:24:26 +0200

    Update Launch.ps1

--- Alteon/CLI/Standalone/SingleIP/new-network/Launch.ps1
+++ Alteon/CLI/Standalone/SingleIP/new-network/Launch.ps1
@@ -1,3 +1,3 @@
-$alteonsingledeployment = Invoke-WebRequest https://raw.githubusercontent.com/Radware/Alteon-Azure-powershell-deployment/master/app/AlteonPS.ps1
+$alteonsingledeployment = Invoke-WebRequest https://raw.githubusercontent.com/Radware/Radware-azure-arm-templates/master/Alteon/CLI/Standalone/SingleIP/new-network/app/AlteonPS.ps1
 Invoke-Expression $($alteonsingledeployment.Content)
 Remove-Variable -Name alteonsingledeployment;
