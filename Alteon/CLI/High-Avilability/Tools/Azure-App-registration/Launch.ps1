aaf1736ab6514d0e9e8e8843c10327dfb5817980
Or Zidkani <31033101+OrZidkani@users.noreply.github.com>
2019-01-07 11:07:03 +0200

    Update Launch.ps1

--- Alteon/CLI/High-Avilability/Tools/Azure-App-registration/Launch.ps1
+++ Alteon/CLI/High-Avilability/Tools/Azure-App-registration/Launch.ps1
@@ -1,3 +1,3 @@
-$AzureAppReg = Invoke-WebRequest https://raw.githubusercontent.com/Radware/Azure-App-registration/master/app/AppReg.ps1
+$AzureAppReg = Invoke-WebRequest https://raw.githubusercontent.com/Radware/Radware-azure-arm-templates/master/Alteon/CLI/High-Avilability/Tools/Azure-App-registration/app/AppReg.ps1
 Invoke-Expression $($AzureAppReg.Content)
 Remove-Variable -Name AzureAppReg;
